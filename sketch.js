bd555cead908e37b27b3dd2f6050ee179492b649
mellany01 <00001065303555sp@al.educacao.sp.gov.br>
2024-05-17 08:35:06 -0300

    Add files via upload
    Projeto artístico com java script

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 600);
+  background (``black``) ;
+}
+ function setup () {
+    stroke ("blue") ;
+   fill ("red") ;
+   if (mouse Is pressed) {
+     red (mouse X, mouse y, 20, 35);
+   }
+ }
+   
